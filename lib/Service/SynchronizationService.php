29ffb958a3f4b3ca8bc966849e872b9dc6771ad1
Ruben van der Linde <ruben@conduction.nl>
2025-06-14 08:33:28 +0200

    Small endpoint fix

--- lib/Service/SynchronizationService.php
+++ lib/Service/SynchronizationService.php
@@ -2406,7 +2406,13 @@ class SynchronizationService
 				$filename = $endpoint['filename'];
 			}
 
-			return $endpoint['endpoint'];
+			// Check if endpoint exists before returning it
+			if (isset($endpoint['endpoint']) === true) {
+				return $endpoint['endpoint'];
+			}
+
+			// If no endpoint is found, return null
+			return null;
 		}
 
 		return $endpoint;
