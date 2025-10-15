f673cb620b83809b0bf3a55386a1f2b165907c77
Robert Zondervan <robert@conduction.nl>
2025-10-15 11:42:13 +0200

    Fix not deleting documents when all documents are removed from an object

--- lib/Service/SynchronizationService.php
+++ lib/Service/SynchronizationService.php
@@ -2739,6 +2739,10 @@ class SynchronizationService
 			return "Not array";
 		}
 
+        if ($array === []) {
+            return 'Empty array';
+        }
+
 		// Check for an associative array
 		if (array_keys($array) !== range(0, count($array) - 1)) {
 			return "Associative array";
@@ -2932,11 +2936,10 @@ class SynchronizationService
                     }
                     break;
 
+                    // Array is empty
+                case 'Empty array':
                 // Array of object(s) that has file(s) - use cleanup logic
                 case "Multidimensional array":
-                    $this->processMultipleFilesWithCleanup($source, $config, $endpoint, $objectId);
-                    break;
-
                 // Array of just endpoints - use cleanup logic
                 case "Indexed array":
                     $this->processMultipleFilesWithCleanup($source, $config, $endpoint, $objectId);
@@ -3653,6 +3656,12 @@ class SynchronizationService
 	private function processMultipleFilesWithCleanup(Source $source, array $config, array $endpoints, ?string $objectId = null): void
 	{
 		$newFileNames = [];
+
+        if ($endpoints === [] && $objectId !== null) {
+            $targetObjectId = $objectId;
+        } elseif ($endpoints === []) {
+            return;
+        }
 
 		// Process all files first and collect their filenames
 		foreach ($endpoints as $endpoint) {
