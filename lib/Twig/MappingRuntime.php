b0aa9f10623dbe815c9df52ab446f51fb745f6b5
Robert Zondervan <robert@conduction.nl>
2025-12-15 16:52:27 +0100

    Add functionality to Twig
    - Call sources from mapping
    - Add encoding and decoding base64
    - Add decoding json

--- lib/Twig/MappingRuntime.php
+++ lib/Twig/MappingRuntime.php
@@ -5,7 +5,9 @@ namespace OCA\OpenConnector\Twig;
 use OCA\OpenConnector\Db\Mapping;
 use OCA\OpenConnector\Db\MappingMapper;
 use OCA\OpenConnector\Db\Source;
+use OCA\OpenConnector\Db\SourceMapper;
 use OCA\OpenConnector\Service\AuthenticationService;
+use OCA\OpenConnector\Service\CallService;
 use OCA\OpenConnector\Service\MappingService;
 use Twig\Extension\RuntimeExtensionInterface;
 use Symfony\Component\Uid\Uuid;
@@ -15,10 +17,66 @@ class MappingRuntime implements RuntimeExtensionInterface
 {
 	public function __construct(
 		private readonly MappingService $mappingService,
-		private readonly MappingMapper  $mappingMapper
+		private readonly MappingMapper  $mappingMapper,
+        private readonly CallService $callService,
+        private readonly SourceMapper $sourceMapper,
 	) {
 
 	}
+
+    /**
+     * Encodes a string to base64.
+     *
+     * @param string $input The unencoded input.
+     * @return string The encoded output.
+     */
+    public function b64enc(string $input): string
+    {
+        return base64_encode($input);
+
+    }//end b64enc()
+
+    /**
+     * Decodes a base64 encoded string to an unencoded string.
+     *
+     * @param string $input The encoded input.
+     * @return string The decoded output.
+     */
+    public function b64dec(string $input): string
+    {
+        return base64_decode($input);
+
+    }//end b64dec()
+
+    public function json_decode(string $input): array
+    {
+        return json_decode($input, associative: true);
+    }
+
+    /**
+     * Call source of given id or reference
+     *
+     * @param array $array The array to turn into a dot array.
+     *
+     * @return array The dot aray.
+     */
+    public function callSource(string $sourceId, string $endpoint, string $method='GET', array $configuration=[], bool $decode=true): array|string
+    {
+        $source = $this->sourceMapper->find($sourceId);
+
+        if (str_contains($endpoint, $source->getLocation()) === true) {
+            $endpoint = substr($endpoint, strlen($source->getLocation()));
+        }
+
+        $response = $this->callService->call($source, $endpoint, $method, $configuration);
+
+        if ($decode === false) {
+            return $response->getResponse()['body'];
+        }
+
+        return $response->getResponse()['body'];
+
+    }//end call()
 
 	/**
 	 * Execute a mapping with given parameters.
--- lib/Twig/MappingRuntimeLoader.php
+++ lib/Twig/MappingRuntimeLoader.php
@@ -3,6 +3,8 @@
 namespace OCA\OpenConnector\Twig;
 
 use OCA\OpenConnector\Db\MappingMapper;
+use OCA\OpenConnector\Db\SourceMapper;
+use OCA\OpenConnector\Service\CallService;
 use OCA\OpenConnector\Service\MappingService;
 use Twig\Extension\RuntimeExtensionInterface;
 use Twig\RuntimeLoader\RuntimeLoaderInterface;
@@ -11,7 +13,9 @@ class MappingRuntimeLoader implements RuntimeLoaderInterface
 {
 	public function __construct(
 		private readonly MappingService $mappingService,
-		private readonly MappingMapper  $mappingMapper
+		private readonly MappingMapper  $mappingMapper,
+        private readonly CallService $callService,
+        private readonly SourceMapper $sourceMapper,
 	) {
 
 	}
@@ -19,7 +23,7 @@ class MappingRuntimeLoader implements RuntimeLoaderInterface
 	public function load(string $class): ?MappingRuntime
 	{
 		if ($class === MappingRuntime::class) {
-			return new MappingRuntime(mappingService: $this->mappingService, mappingMapper: $this->mappingMapper);
+			return new MappingRuntime(mappingService: $this->mappingService, mappingMapper: $this->mappingMapper, callService: $this->callService, sourceMapper: $this->sourceMapper);
 		}
 
 		return null;
