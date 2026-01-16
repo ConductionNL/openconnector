ef8357d66db79d3cab78c588ad680f2aeee5cfe6
Robert Zondervan <robert@conduction.nl>
2026-01-16 09:49:03 +0100

    Set synchronization trigger when syncing from rule

--- lib/Service/EndpointService.php
+++ lib/Service/EndpointService.php
@@ -1675,6 +1675,9 @@ class EndpointService
 
             $this->objectService->getOpenRegisters()->clearCurrents();
             $fetchedObject = $this->objectService->getOpenRegisters()->find($id);
+            $foData = $fetchedObject->jsonSerialize();
+            $foData['synchronization_trigger'] = true;
+            $fetchedObject->setObject($foData);
         }
 
 
